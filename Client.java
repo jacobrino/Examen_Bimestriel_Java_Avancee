9c931346a47ecf0f86d488f4e8b4c3b7060afb79
jacob_rino <jacobrino05@gmail.com>
2025-02-23 18:48:14 +0300

    Add ticket list in Client attributes for 'Passer' relation

--- Client.java
+++ Client.java
@@ -1,17 +1,22 @@
+import java.util.ArrayList;
+
 public class Client 
 {
     private int idc;
     private String nomc;
-    
+    private ArrayList<Ticket> tickets;
+
     public Client() 
     {
         this.idc = 0;
         this.nomc = "";
+        this.tickets=null;
     }
-    public Client(int idc, String nomc) 
+    public Client(int idc, String nomc, ArrayList<Ticket> tickets) 
     {
         this.idc = idc;
         this.nomc = nomc;
+        this.tickets=tickets;
     }
     public int getIdc() 
     {
@@ -20,6 +25,10 @@ public class Client
     public String getNomc() 
     {
         return this.nomc;
+    }
+    public ArrayList<Ticket> getTickets()
+    {
+        return this.tickets;
     }
 
     public void setIdc(int idc) 
@@ -30,5 +39,8 @@ public class Client
     {
         this.nomc = nomc;
     }
-
+    public void setTickets(ArrayList<Ticket> tickets)
+    {
+        this.tickets=tickets;
+    }
 }
