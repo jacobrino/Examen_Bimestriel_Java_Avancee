d07df482325bb85670e6f59be4c81e36ae36a690
jacob_rino <jacobrino05@gmail.com>
2025-02-23 19:28:47 +0300

    Add class Connexion to pilot database for a package

---
+++ Connexion.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connexion 
+{
+    private String url = "jdbc:mysql://127.0.0.1/test_java_avance";
+    private String utilisateur = "phpmyadmin";
+    private String passe = "000";
+    // Le mot de passe est juste pour test, mais normallement en terme de sécurité, on ne met pas de mot de passe aussi simple que ça.
+
+    private static Connection cx=null;
+    
+    private Connexion() 
+    {
+        try 
+        {
+            Connexion.cx = DriverManager.getConnection(url, utilisateur, passe);
+        } 
+        catch(SQLException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getInstance() 
+    {
+        if(cx == null) 
+        {
+            new Connexion();
+        }
+        return cx;
+    }
+}
