5e383cdf2bc757eba9c9a43a73a1fd2f6575a9c4
jacob_rino <jacobrino05@gmail.com>
2025-02-23 18:40:32 +0300

    Add idf in Produit attributes for 'Fournir' relation and it's all about n relation

--- Produit.java
+++ Produit.java
@@ -5,19 +5,22 @@ public class Produit
     private double prix;
     // On a mis la variable prix en double car un prix peut avoir une virgule. Comme 200,95 ariary
     private int quantiteStock;
+    private int idf;
     public Produit() 
     {
         this.idp = 0;
         this.designation = "";
         this.prix = 0.0;
         this.quantiteStock=0;
+        this.idf=0;
     }
-    public Produit(int idp, String designation, double prix, int quantiteStock) 
+    public Produit(int idp, String designation, double prix, int quantiteStock, int idf) 
     {
         this.idp = idp;
         this.designation = designation;
         this.prix = prix;
         this.quantiteStock=quantiteStock;
+        this.idf=idf;
     }
     public int getIdp() 
     {
@@ -34,6 +37,10 @@ public class Produit
     public int getQuantiteStock()
     {
         return this.quantiteStock;
+    }
+    public int getIdf()
+    {
+        return this.idf;
     }
     
     public void setIdp(int idp) 
@@ -52,6 +59,9 @@ public class Produit
     {
         this.quantiteStock=quantiteStock;
     }
-    
+    public void setIdf(int idf)
+    {
+        this.idf=idf;
+    }
     
 }
