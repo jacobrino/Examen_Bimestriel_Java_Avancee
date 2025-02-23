6395174c5647977b54d07c0199bf06363bc0691d
jacob_rino <jacobrino05@gmail.com>
2025-02-23 19:31:29 +0300

    Test first connection for Connexion class, he is success

--- Main.java
+++ Main.java
@@ -1,28 +1,43 @@
-import java.time.LocalTime;
-import java.time.LocalDate;
+import java.sql.Connection;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         
-        // Modèle logique object finish
+        // Toutes les classes du modèle logique object finish sur le commit [develop b9d9456]
 
-
-        
         System.out.println("Bonjour java\n\n");
-   
-   
-        // Créer une heure spécifique
-        LocalTime heureSpecifique = LocalTime.of(14, 30, 45);
-        // Hours/Minutes/Seconds
-        System.out.println("Heure spécifique : " + heureSpecifique);
 
+        Connection test=Connexion.getInstance();
 
-        // Créer une date spécifique
-        LocalDate dateSpecifique = LocalDate.of(2025, 2, 23);
-        // Years/Months/Days
+        if (test != null) 
+        {
+            System.out.println("Connexion réussi");
+        }
+        else
+        {
+            System.out.println("Connexion non réussi");
+        }
+        
+        System.out.println("java finish\n\n");
 
-        System.out.println("Date spécifique : " + dateSpecifique);
-    
+        
+
 
     }
+
+    /*
+
+        Pour compiler et lancer on doit faire la procédure suivante, sinon, ça ne marche pas, car le package mysql-connector n'est pas 
+        dans le classpath de notre java(jre, jdk).
+        Dans le cas contraire, il faut utiliser un gestionnaire de dépendance (gradle, maven)
+        
+        ------------------------------------------------------------------------------------------------------------------------------
+        javac -cp ".:mysql-connector-j-9.2.0.jar" Main.java 
+
+        java -cp ".:mysql-connector-j-9.2.0.jar" Main 
+
+        Il faut que le fichier mysql-connector-j-9.2.0.jar se trouve dans le même espace de travail que ce fichier
+
+    */
 }
